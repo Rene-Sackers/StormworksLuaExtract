35df919e96db7d78bee95410aa38873af7b909fa
René Sackers <rene.sackers@live.com>
2019-05-18 13:37:32 +0200

    Backup lua files before overwriting

--- src/StormworksLuaExtract/Services/XmlToLocalLuaWriteService.cs
+++ src/StormworksLuaExtract/Services/XmlToLocalLuaWriteService.cs
@@ -11,12 +11,25 @@ namespace StormworksLuaExtract.Services
 		{
 			Console.WriteLine($"Extracting Lua scripts from microcontroller '{luaScript.MicrocontrollerXmlPath}'");
 			
-			if (File.Exists(luaScript.LuaFilePath) && FileHelper.NoTouchReadFile(luaScript.LuaFilePath) == luaScript.Script)
+			if (File.Exists(luaScript.LuaFilePath))
 			{
-				Console.WriteLine($"Nothing changed for script {luaScript.ObjectId} from microcontroller {luaScript.MicrocontrollerName}.");
-				return;
+				var currentScript = FileHelper.NoTouchReadFile(luaScript.LuaFilePath);
+
+				if (currentScript == luaScript.Script)
+				{
+					Console.WriteLine($"Nothing changed for script {luaScript.ObjectId} from microcontroller {luaScript.MicrocontrollerName}.");
+					return;
+				}
+
+				// Backup
+				var backupFilePath = Path.Join(Statics.LocalBackupDirectory, $"{luaScript.MicrocontrollerName}_{luaScript.ObjectId} {DateTime.Now:yyyy-MM-dd HH-mm-ss}.lua");
+				if (!FileHelper.TryWriteFile(backupFilePath, currentScript))
+					return;
+
+				Console.WriteLine($"Wrote backup to {backupFilePath}");
 			}
-				
+
+
 			FileHelper.TryWriteFile(luaScript.LuaFilePath, luaScript.Script);
 
 			Console.WriteLine($"Wrote script {luaScript.ObjectId} from microcontroller {luaScript.MicrocontrollerName} to {luaScript.LuaFilePath}.");

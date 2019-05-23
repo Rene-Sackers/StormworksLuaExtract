aef3e71379e959feecf7a1c6046c8b7e7f6fc960
René Sackers <rene.sackers@live.com>
2019-05-23 21:24:34 +0200

    Added lua minify node API service

---
+++ src/LuaMinifyApi/index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var luamin = require("luamin");
+
+http.createServer(function (req, res) {
+	let lua = '';
+	req.on('data', chunk => {
+		lua += chunk.toString(); // convert Buffer to string
+	});
+	req.on('end', () => {
+		var minified = luamin.minify(lua);
+
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.write(minified);
+		res.end();
+	});
+
+}).listen(8080);

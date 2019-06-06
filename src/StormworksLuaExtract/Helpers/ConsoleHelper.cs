f4f1451992888cb713b4f476228ed8eae0caf920
René Sackers <rene.sackers@live.com>
2019-06-06 18:57:57 +0200

    Fixes #2

--- src/StormworksLuaExtract/Helpers/ConsoleHelper.cs
+++ src/StormworksLuaExtract/Helpers/ConsoleHelper.cs
@@ -4,13 +4,13 @@ namespace StormworksLuaExtract.Helpers
 {
 	public static class ConsoleHelper
 	{
+		private static readonly ConsoleColor DefaultConsoleColor = Console.ForegroundColor;
+
 		public static void WriteWarning(object value)
 		{
-			var previousForegroundColor = Console.ForegroundColor;
-
 			Console.ForegroundColor = ConsoleColor.Red;
 			Console.WriteLine(value);
-			Console.ForegroundColor = previousForegroundColor;
+			Console.ForegroundColor = DefaultConsoleColor;
 		}
 	}
 }
